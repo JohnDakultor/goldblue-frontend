a861246ff4734b596f6e5ea2366e9c31b1620b1e
Germel Marata <germeljohnmarata@gmail.com>
2024-10-27 05:58:11 +0800

    changed video on dashboad

--- src/model/productPages/Dashboard.jsx
+++ src/model/productPages/Dashboard.jsx
@@ -19,7 +19,7 @@ import img1 from "../../assets/1.jpeg";
 import img2 from "../../assets/2.jpg";
 import img3 from "../../assets/3.jpg";
 import img4 from "../../assets/4.jpg";
-import videoSrc from "../../assets/GoldBlue.mp4"; // Import the video
+import videoSrc from "../../assets/goldbluevid.mp4"; // Import the video
 import { getTransactions, getWithdrawTransactions } from "../../services/Axios";
 import "../../App.css";
 
